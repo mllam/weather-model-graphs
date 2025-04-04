5d98d22da5cee3dd0d2fbc06e7a3fe27efaa22f2
Leif Denby <leif@denby.eu>
2025-04-04 22:10:47 +0200

    first implementation of graph filters

--- src/weather_model_graphs/__init__.py
+++ src/weather_model_graphs/__init__.py
@@ -1,4 +1,5 @@
 from . import create, save, visualise
+from .filtering import filter_graph
 from .networkx_utils import (
     replace_node_labels_with_unique_ids,
     split_graph_by_edge_attribute,
