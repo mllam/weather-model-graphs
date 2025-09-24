68cf065c7ad4b135f406d96e45cd43ff3f952cbf
joeloskarsson <joel.oskarsson@liu.se>
2025-09-24 11:26:40 +0200

    Complete decoder mask test

--- tests/test_graph_decode_gridpoints_mask.py
+++ tests/test_graph_decode_gridpoints_mask.py
@@ -8,12 +8,6 @@ import weather_model_graphs as wmg
 from tests import utils as test_utils
 
 
-def load_adjecency_matrix(graph_name, output_directory="."):
-    fp = Path(output_directory) / f"{graph_name}_edge_index.pt"
-    edge_index = torch.load(fp)
-    return edge_index.numpy()
-
-
 def test_graph_decode_gridpoints_mask():
     """
     Test to ensure that when applying a mask to select which grid nodes to
@@ -25,18 +19,28 @@ def test_graph_decode_gridpoints_mask():
     mesh_node_distance = 2.5
     fn = wmg.create.archetype.create_keisler_graph
 
-    unfiltered_graph = fn(coords=xy, mesh_node_distance=mesh_node_distance)
+    unfiltered_graph = fn(
+        coords=xy, mesh_node_distance=mesh_node_distance, return_components=True
+    )["m2g"]
 
-    # mask every 3rd gridpoint
-    decode_mask = (np.arange(xy.shape[0]) % 3 == 0).astype(int)
+    # keep only every 3rd gridpoint
+    decode_mask = np.arange(xy.shape[0]) % 3 == 0
     filtered_graph = fn(
-        coords=xy, mesh_node_distance=mesh_node_distance, decode_mask=decode_mask
-    )
+        coords=xy,
+        mesh_node_distance=mesh_node_distance,
+        decode_mask=decode_mask,
+        return_components=True,
+    )["m2g"]
+
+    def load_edge_index(graph_name, output_directory="."):
+        file_path = Path(output_directory) / f"{graph_name}_edge_index.pt"
+        edge_index = torch.load(file_path)
+        return edge_index.numpy()
 
     # store the graphs to disk and load the adjecency matrices for each
     with tempfile.TemporaryDirectory() as tmpdirname:
-        name_filtered = "example_keisler_graph_filtered"
-        name_unfiltered = "example_keisler_graph"
+        name_filtered = "g2m_filtered"
+        name_unfiltered = "g2m"
 
         wmg.save.to_pyg(
             graph=unfiltered_graph, output_directory=tmpdirname, name=name_unfiltered
@@ -45,31 +49,40 @@ def test_graph_decode_gridpoints_mask():
             graph=filtered_graph, output_directory=tmpdirname, name=name_filtered
         )
 
-        adj_filtered = load_adjecency_matrix(name_filtered, output_directory=tmpdirname)
-        adj_unfiltered = load_adjecency_matrix(
-            name_unfiltered, output_directory=tmpdirname
+        adj_filtered = load_edge_index(name_filtered, output_directory=tmpdirname)
+        adj_unfiltered = load_edge_index(name_unfiltered, output_directory=tmpdirname)
+
+    # Look up which edges should be kept according to decode_mask
+    unfiltered_edge_mask = decode_mask[adj_unfiltered[1]]
+    # Filter edges from full set
+    adj_unfiltered_masked = adj_unfiltered[:, unfiltered_edge_mask]
+
+    np.testing.assert_equal(adj_filtered.shape, adj_unfiltered_masked.shape)
+
+    # Re-index nodes from unfiltered g2m to match
+    # New index is number of kept nodes before in decode_mask
+    reindex_map = decode_mask.cumsum() - 1
+    adj_grid_reindexed = reindex_map[adj_unfiltered_masked[1]]
+
+    # Reindex mesh nodes to start at 0 and sort edge index tensor
+    # before comparing. Checks that these represent the same graph.
+    reindexed_wmg_filtered = np.sort(
+        np.concatenate(
+            (
+                adj_filtered[0] - adj_filtered[0].min(),
+                adj_filtered[1],
+            ),
+            axis=0,
         )
+    )
+    reindexed_post_filtered = np.sort(
+        np.concatenate(
+            (
+                adj_unfiltered_masked[0] - adj_unfiltered_masked[0].min(),
+                adj_grid_reindexed,
+            ),
+            axis=0,
+        )
+    )
 
-    # check that the edges in the graph objects match the adjency matrices that
-    # have been written
-    graph_edges_unfiltered = np.array(unfiltered_graph.edges)
-    assert set(zip(*graph_edges_unfiltered.T)) == set(zip(*adj_unfiltered))
-    graph_edges_filtered = np.array(filtered_graph.edges)
-    assert set(zip(*graph_edges_filtered.T)) == set(zip(*adj_filtered))
-
-    # manually filter the edge connections to the grid-nodes that are masked
-    # out and create an adjency matrix
-    grid_indexes_to_remove = np.arange(0, xy.shape[0])[decode_mask == 0]
-    adj_pairs = []
-    for i in range(adj_unfiltered.shape[1]):
-        m_idx, g_idx = adj_unfiltered[:, i]
-        if g_idx in grid_indexes_to_remove:
-            continue
-        adj_pairs.append((m_idx, g_idx))
-    adj_unfiltered_masked = np.array(adj_pairs).T
-
-    import ipdb
-
-    ipdb.set_trace()
-
-    np.testing.assert_equal(adj_filtered, adj_unfiltered_masked)
+    np.testing.assert_equal(reindexed_wmg_filtered, reindexed_post_filtered)
