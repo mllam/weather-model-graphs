2a62aa4631f48b490023bc5db50bf28f0215c517
Shristi Goel <shristigoel2005@gmail.com>
2026-02-23 22:53:37 +0530

    Remove unrelated __version__ addition

--- src/weather_model_graphs/__init__.py
+++ src/weather_model_graphs/__init__.py
@@ -3,9 +3,3 @@ from .networkx_utils import (
     replace_node_labels_with_unique_ids,
     split_graph_by_edge_attribute,
 )
-import importlib.metadata
-
-try:
-    __version__ = importlib.metadata.version(__name__)
-except importlib.metadata.PackageNotFoundError:
-    __version__ = "unknown"
